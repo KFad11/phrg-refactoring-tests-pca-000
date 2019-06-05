81cbf48b2411163fc0ec6eefe07271dc148a6f75
KFad <kfadigan@yahoo.com>
2019-06-04 21:32:00 -0400

    Refactored the tests from let into factories

---
+++ spec/factories/post.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :post do
+    title "Try and Guess the Top Secret plan I have, you Won't Believe it"
+    content "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet luctus felis. Sed in leo sed erat accumsan interdum. Vivamus orci dolor, sagittis vitae tempor luctus, ullamcorper ut urna. In in turpis in odio porttitor porttitor. Integer sed."
+    summary "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet luctus felis. Sed in leo sed erat accumsan interdum. Vivamus orci dolor, sagittis vitae tempor luctus, ullamcorper ut urna. In in turpis in odio porttitor porttitor. Integer sed."
+    category "Fiction"
+  end
+end
